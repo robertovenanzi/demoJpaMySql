ee05d339f2f9f7aaee3fd95cb3d5894505ee97ea
Roberto Venanzi <roberto.venanzi@gmail.com>
2015-11-26 15:42:26 +0100

    secondo commit

--- src/main/java/com/example/domain/OptionEntity.java
+++ src/main/java/com/example/domain/OptionEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Entity(name = "optionEntity")
 @Table(name = "OPTION_TABLE")
 public class OptionEntity {
 	
